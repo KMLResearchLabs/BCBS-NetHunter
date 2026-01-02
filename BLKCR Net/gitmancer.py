06f66730be7b7373f46f648f472ea3309149f7ac
KML Research Labs <kmlresearchlabs@gmail.com>
2026-01-02 17:19:33 -0300

    GitMancer Update

--- BLKCR Net/gitmancer.py
+++ BLKCR Net/gitmancer.py
@@ -1,6 +1,8 @@
 def gitmancer():
     import subprocess
 
+    #Sys
+
     git_fetch = subprocess.run(['git', 'fetch'], cwd = '..', capture_output=True, text=True)
     print(git_fetch.stdout)
 
@@ -9,3 +11,17 @@ def gitmancer():
 
     git_pull = subprocess.run(['git', 'pull'], cwd = '..', capture_output=True, text=True)
     print(git_pull.stdout)
+
+    #Python lib
+
+    pip_requests = subprocess.run(['pip', 'install', 'requests'], cwd = '..', capture_output=True, text=True)
+    print(pip_requests.stdout)
+
+    pip_dnspython = subprocess.run(['pip', 'install', 'dnspython'], cwd = '..', capture_output=True, text=True)
+    print(pip_dnspython.stdout)
+
+    pip_pythonwhois = subprocess.run(['pip', 'install', 'python-whois'], cwd = '..', capture_output=True, text=True)
+    print(pip_pythonwhois.stdout)
+
+    pip_phonenumbers = subprocess.run(['pip', 'install', 'phonenumbers'], cwd = '..', capture_output=True, text=True)
+    print(pip_phonenumbers.stdout)
