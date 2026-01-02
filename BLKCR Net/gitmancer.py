e791adc13cf7a12b330117ea7dfe67411462d1e4
KML Research Labs <kmlresearchlabs@gmail.com>
2026-01-02 12:04:29 -0300

    GitMancer bug fix

--- BLKCR Net/gitmancer.py
+++ BLKCR Net/gitmancer.py
@@ -1,11 +1,11 @@
 def gitmancer():
     import subprocess
 
-    git_fetch = subprocess.run(['git', 'fetch'], cwd = 'BCBS-NetHunter', capture_output=True, text=True)
+    git_fetch = subprocess.run(['git', 'fetch'], cwd = '..', capture_output=True, text=True)
     print(git_fetch.stdout)
 
-    git_status = subprocess.run(['git', 'status'], cwd = 'BCBS-NetHunter', capture_output=True, text=True)
+    git_status = subprocess.run(['git', 'status'], cwd = '..', capture_output=True, text=True)
     print(git_status.stdout)
 
-    git_pull = subprocess.run(['git', 'pull'], cwd = 'BCBS-NetHunter', capture_output=True, text=True)
+    git_pull = subprocess.run(['git', 'pull'], cwd = '..', capture_output=True, text=True)
     print(git_pull.stdout)
