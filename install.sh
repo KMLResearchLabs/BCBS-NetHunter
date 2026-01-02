1e7438f74514d0bc4abb2b6e7d2a1417c7bd3383
KML Research Labs <kmlresearchlabs@gmail.com>
2026-01-02 19:39:30 -0300

    Termux Command test

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "[+] Instaling BCBS-NetHunter..."
+
+chmod +x bcbs
+chmod +x terminal.py
+
+cp bcbs $PREFIX/bin/bcbslauncher
+
+echo "[ OK ] BCBS NetHunter is ready to use"
+echo "To start type: bcbs"
