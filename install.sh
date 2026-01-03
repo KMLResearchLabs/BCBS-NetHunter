f1b69c97c9c1541f6d636e004ed8a571a7598f54
KML Research Labs <kmlresearchlabs@gmail.com>
2026-01-02 21:06:06 -0300

    Final Install

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
+echo "[+] Getting BCBS-NetHunter Launcher..."
+
 chmod +x bcbslauncher.sh
 
+echo "[+] Giving permission..."
+
 cp bcbslauncher.sh $PREFIX/bin/bcbs
+
+echo "[+] Setting command..."
+
+echo "[ OK ] Command installation finished"
+echo "Type "bcbs" to inicializate BCBS-NetHunter"
 
 hash -r
