a7ec45795949a32356b4894b16c18c9e4652d662
KML Research Labs <kmlresearchlabs@gmail.com>
2026-01-06 12:11:35 -0300

    Bug Fix

--- BLKCR Net/terminal_commands/WifiBF.py
+++ BLKCR Net/terminal_commands/WifiBF.py
@@ -92,7 +92,7 @@ def WifiBF():
 
     print(f"[+] Cracking '{ssid}'...")
 
-    with open(r'MrPaper.txt', 'r') as f:
+    with open(r'terminal_commands/MrPaper.txt', 'r') as f:
         while True:
             line = f.readline()
             if not line:
@@ -110,4 +110,5 @@ def WifiBF():
                 print("[ OK ] Cracked!")
                 print(f"[+] {ssid} password: {passwd}")
                 break
+
 
